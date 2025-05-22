6d8c1180a699b7885b4260c8379b0b71d0149b14
Daniel <mills.daniel320@gmail.com>
2025-05-21 23:05:24 -0500

    added a confirmations when contact btn is clicked

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
 $('#showMore').on('click', () => $('#hiddenText').toggle())
 
 $('#contact').on('click', () => console.log('contact clicked'))
+
+$('#contactBtn').on('click', () => $('#confirmation').text('Confirmed!').css('color', 'green'))
