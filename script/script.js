eab9976ed0c36d89c12f054d6866ea7a3442ba13
Daniel <mills.daniel320@gmail.com>
2025-05-21 23:40:21 -0500

    Added basic form validation

--- script/script.js
+++ script/script.js
@@ -2,4 +2,10 @@ $('#showMore').on('click', () => $('#hiddenText').toggle())
 
 $('#contact').on('click', () => console.log('contact clicked'))
 
-$('#contactBtn').on('click', () => $('#confirmation').text('Confirmed!').css('color', 'green'))
+$('#contactBtn').on('click', () => {
+    if($('#firstName').val() === '' || $('#lastName').val() === '' || $('#phone').val() === '') {
+        $('#confirmation').text('Please Fill Out the Form').css('color', 'red')
+    } else {
+        $('#confirmation').text('Confirmed!').css('color', 'green')
+    }
+})
