e7b06b96f1820f8ff0069749eaa8cd2985fcc8f2
Daniel <mills.daniel320@gmail.com>
2025-05-25 17:15:59 -0500

    Removed Shadow Dom From Custome Element, Added Instance Events and Custom modal message depending on profile clicked.

--- script/script.js
+++ script/script.js
@@ -6,25 +6,40 @@ class ProfileCard extends HTMLElement {
 
         this.appendChild(template)
 
+        const $card = $(this)
+        const $button = $card.find('.showMore')
+        const $contact = $card.find('.contact')
+        const $hidden = $card.find('.hiddenText')
+
+        $button.on('click', () => $hidden.toggle())
+        $contact.on('click', () => {
+            const profileName = this.getAttribute('name')
+
+            $('.modal').attr('data-profile', profileName)
+
+            console.log(profileName, $('.modal').data('targetPerson', profileName))
+        })
+
         this.querySelector('.card-text').textContent = this.getAttribute('bio')
         this.querySelector('.card-title').textContent = this.getAttribute('name')
         this.querySelector('.card-img-top').src = this.getAttribute('img')
-        this.querySelector('#hiddenText').textContent = this.getAttribute('hidden-text')
-        this.querySelector('#showMore').textContent = this.getAttribute('show-more')
-        this.querySelector('#contact').textContent = this.getAttribute('contact')
+        this.querySelector('.hiddenText').textContent = this.getAttribute('hidden-text')
+        this.querySelector('.showMore').textContent = this.getAttribute('show-more')
+        this.querySelector('.contact').textContent = this.getAttribute('contact')
     }
 }
 
 customElements.define('profile-card', ProfileCard)
 
-$('#showMore').on('click', () => $('#hiddenText').toggle())
 
-$('#contact').on('click', () => console.log('contact clicked'))
 
 $('#contactBtn').on('click', () => {
+    const person = $('.modal').data('profile')
+    console.log($('.modal').data())
+
     if($('#firstName').val() === '' || $('#lastName').val() === '' || $('#phone').val() === '') {
         $('#confirmation').text('Please Fill Out the Form').css('color', 'red')
     } else {
-        $('#confirmation').text('Confirmed!').css('color', 'green')
+        $('#confirmation').text(`Confirmed! Message sent to ${person}`).css('color', 'green')
     }
 })
