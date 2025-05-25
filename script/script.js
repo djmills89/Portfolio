4dc029b9eba4f8e903edd17725c59aeb8f400cdd
Daniel <mills.daniel320@gmail.com>
2025-05-25 16:18:58 -0500

    Created a reusable profile card web component

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
+// Porfile Card Web Component
+class ProfileCard extends HTMLElement {
+    constructor() {
+        super()
+        const template = document.getElementById('profileCardTemplate').content.cloneNode(true)
+
+        this.appendChild(template)
+
+        this.querySelector('.card-text').textContent = this.getAttribute('bio')
+        this.querySelector('.card-title').textContent = this.getAttribute('name')
+        this.querySelector('.card-img-top').src = this.getAttribute('img')
+        this.querySelector('#hiddenText').textContent = this.getAttribute('hidden-text')
+        this.querySelector('#showMore').textContent = this.getAttribute('show-more')
+        this.querySelector('#contact').textContent = this.getAttribute('contact')
+    }
+}
+
+customElements.define('profile-card', ProfileCard)
+
 $('#showMore').on('click', () => $('#hiddenText').toggle())
 
 $('#contact').on('click', () => console.log('contact clicked'))
